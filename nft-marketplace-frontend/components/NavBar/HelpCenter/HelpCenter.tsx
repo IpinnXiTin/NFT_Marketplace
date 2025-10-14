38ecaff8fa4844b84acea00cd2f4b3aeef5a4427
Tran Thang <thang-tv@kinjiro-vn.com>
2025-10-14 17:33:18 +0700

    feat(navbar): implement navbar for NFT Marketplace

--- nft-marketplace-frontend/components/NavBar/HelpCenter/HelpCenter.tsx
+++ nft-marketplace-frontend/components/NavBar/HelpCenter/HelpCenter.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+
+// INTERNAL IMPORT
+import Style from './HelpCenter.module.css';
 
 const HelpCenter = () => {
+
+  const helpCenter = [
+    {
+      name: "About",
+      link: "about",
+    },
+    {
+      name: "Contact us",
+      link: "contact-us",
+    },
+    {
+      name: "Sign Up",
+      link: "sign-up",
+    },
+    {
+      name: "Sign In",
+      link: "sign-in",
+    },
+    {
+      name: "Subscription",
+      link: "subscription",
+    },
+  ];
   return (
-    <div>
-      
+    <div className={Style.box}>
+      {
+        helpCenter.map((el, i) => (
+          <div key={i + 1} className={Style.helpCenter}>
+            <Link href={{pathname: `${el.link}`}}>
+              {el.name}
+            </Link>
+          </div>
+        ))
+      }
     </div>
   )
 }
